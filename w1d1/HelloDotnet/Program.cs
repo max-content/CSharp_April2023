16f78ae68cd7b7b9ae138fd340d45768a7975fa9
Max Salzman <mnsalz91@gmail.com>
2023-04-29 17:57:59 -0400

    First C#, HelloDotnet, Fundamentals

---
+++ w1d1/HelloDotnet/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Random rand = new Random();
+for(int i = 1; i <= 10; i++)
+{
+    // Every time the loop executes we will get a NEW random value between 2 and 7
+    Console.WriteLine(rand.Next(2,8));
+}
