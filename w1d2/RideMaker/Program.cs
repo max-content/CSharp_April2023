c7ec1680a5acf15f02483b47cdd2acb863587a31
Max Salzman <75455870+max-content@users.noreply.github.com>
2023-05-03 13:58:36 -0400

    Rider complete

---
+++ w1d2/RideMaker/Program.cs
@@ -0,0 +1,19 @@
+﻿Vehicle MyVehicle = new Vehicle("Car", "blue");
+Vehicle MySkates = new Vehicle("Skates", "white", false, 1);
+Vehicle MyBike = new Vehicle("Bicycle", "pink", false, 2, 67);
+
+List<Vehicle> VehicleList = new List<Vehicle>();
+VehicleList.Add(MyVehicle);
+VehicleList.Add(MySkates);
+VehicleList.Add(MyBike);
+
+foreach (Vehicle item in VehicleList)
+{
+    item.ShowInfo();
+}
+
+MyVehicle.ShowInfo();
+MySkates.ShowInfo();
+
+MyVehicle.Travel(45);
+MyVehicle.Travel(66);
