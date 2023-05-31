d75b571ffaca65f227def8427880ce084661e3d4
Max Salzman <75455870+max-content@users.noreply.github.com>
2023-05-31 12:49:45 -0400

    session workshop styles

--- w1d5/SessionWorkshop/Controllers/HomeController.cs
+++ w1d5/SessionWorkshop/Controllers/HomeController.cs
@@ -15,22 +15,68 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-
+        HttpContext.Session.SetInt32("Counter", 22);
         return View();
     }
 
     [HttpPost("process")]
     public IActionResult Process(string firstName)
     {
-        
-        HttpContext.Session.SetString("FirstName",firstName);
-        return RedirectToAction("Dashboard");
+        if(firstName !=null)
+        {
+            HttpContext.Session.SetString("FirstName",firstName);
+            return RedirectToAction("Dashboard");
+
+        }
+        return RedirectToAction("Index");
     }
+
 
     [HttpGet("dashboard")]
     public IActionResult Dashboard()
     {
+        string? Name = HttpContext.Session.GetString("FirstName");
+        if(Name == null)
+        {
+            RedirectToAction("Index");
+        }
         return View("Dashboard");
+    }
+
+    [HttpPost("process/count")]
+    public IActionResult Increment(string countVar)
+    {
+        Console.WriteLine(countVar);
+        int? CurrentCount = HttpContext.Session.GetInt32("Counter");
+        //if value == 1, 2, -1, random
+        if(countVar == "1")
+        {
+            if(CurrentCount != null) HttpContext.Session.SetInt32("Counter", (int) CurrentCount + 1);
+        } 
+        else if(countVar == "2")
+        {
+            if(CurrentCount != null) HttpContext.Session.SetInt32("Counter", (int) CurrentCount + 2);
+        }
+        else if(countVar == "-1")
+        {
+            if(CurrentCount != null) HttpContext.Session.SetInt32("Counter", (int) CurrentCount - 1);
+        }
+        else
+        {
+            Random RandAlThor = new Random(); //random declaration 
+            int DragonReborn = RandAlThor.Next(10,25); //random instance
+            if(CurrentCount != null) HttpContext.Session.SetInt32("Counter", (int) CurrentCount + DragonReborn);
+            Console.WriteLine(DragonReborn);
+        }
+
+        return RedirectToAction("Dashboard");
+    }
+
+    [HttpPost("logout")]
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Index");
     }
 
     public IActionResult Privacy()
